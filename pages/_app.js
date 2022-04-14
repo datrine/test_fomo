5c04efa2a2fef6c3167fdca260d2c703f56205dc
datrine <trinitietp@gmail.com>
2022-04-14 21:54:29 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import 'w3-css'
 //import './custom.scss';
-import "../comps/temp/src/pages/documentation/charts/chart-columns/custom.scss"
+//import "../comps/temp/src/pages/documentation/charts/chart-columns/custom.scss"
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
