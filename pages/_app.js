d8e6f1a12b5860fd519ad44d99290cff5d8d5699
datrine <trinitietp@gmail.com>
2022-04-14 22:40:34 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import 'w3-css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, ...pageProps }) {
+  console.log("hjghh")
     return <>
       <Component {...pageProps} />
     </>
